0c81027786c361241820e474eed0c834482cd9d6
Dan Trapp <dtrapp@gmail.com>
2020-04-19 15:22:19 -0700

    added gradient filter

--- Audio Chromo/Views/Room.swift
+++ Audio Chromo/Views/Room.swift
@@ -14,6 +14,7 @@ struct Room: View {
     @State var chooseColor = Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1))
     @State var title = ""
     @State var description = ""
+    @State var moods = ""
     
     var body: some View {
         ZStack {
@@ -21,45 +22,55 @@ struct Room: View {
             VStack {
                 
                 //color view
-                 ZStack {
-                    chooseColor.opacity(0.88)
+                ZStack {
+                    chooseColor
                         .edgesIgnoringSafeArea(.all)
+                    LinearGradient(gradient: Gradient(colors: [chooseColor, chooseColor.opacity(0.08), chooseColor.opacity(0.38), chooseColor.opacity(0.18),  Color.white.opacity(0.42)]), startPoint: .top, endPoint: .bottom)
+                    
                     
                     
                     if buttonTapped{
-                    //display title
+                        //display title
                         VStack {
-                        Text(self.title)
-                            .font(.title)
-                            .fontWeight(.bold)
-                        
-                    //description
-                        Text(self.description)
-                        .font(.subheadline)
-                        
-                        
-                        //start button
-                        HStack {
-                            Image(systemName: "play.fill")
-                                .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .font(.system(size: 12, weight: .medium))
-                                                   .frame(width: 36, height: 36)
+                            Text(self.title)
+                                .font(.title)
+                                .fontWeight(.bold)
+                            
+                            //description
+                            Text(self.description)
+                                .font(.subheadline)
+                            
+                            //moods
+                            Text(self.moods)
+                                .font(.caption)
                             
                             
-                            Text("START")
+                            //start button
+                            HStack {
+                                Image(systemName: "play.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .font(.system(size: 12, weight: .medium))
+                                    .frame(width: 36, height: 36)
+                                
+                                
+                                Text("START")
+                            }
+                            
                         }
-                    
+                        .padding(20)
+                        .frame(width: 300, height: 300)
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 50, style: .continuous))
+                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 5)
+                        .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+                        .shadow(color: Color.black.opacity(0.4), radius: 40, x: 0, y: 40)
+                        
                     }
-                    .padding(20)
-                    .frame(width: 300, height: 300)
-                    .background(Color.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 50, style: .continuous))
-                    .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
                     
-                    }
                 }
-               
+                
+                
                 //choose color
                 ScrollView(.horizontal, showsIndicators: false)  {
                     HStack {
@@ -77,25 +88,29 @@ struct Room: View {
                                 //set description
                                 self.description = theColor.description
                                 
+                                //set moods
+                                self.moods = theColor.moods
+                                
+                                
                                 //button tapped
                                 self.buttonTapped = true
                                 
                             }) {
                                 
-                             
-                                    theColor.color
-                                        .frame(width: 60)
-                                        .frame(height: 60).background(Color.black)
-                                        .clipShape(RoundedRectangle(cornerRadius: 50, style: .continuous))
-                                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
-                                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                                
+                                theColor.color
+                                    .frame(width: 60)
+                                    .frame(height: 60).background(Color.black)
+                                    .clipShape(RoundedRectangle(cornerRadius: 50, style: .continuous))
+                                    .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
                                 
                                 
                                 
                             }
-                        //set color picker frame height
-                            .frame(height: 70)
-        
+                                //set color picker frame height
+                                .frame(height: 70)
+                            
                         }
                     }
                 }
@@ -125,64 +140,75 @@ struct Room: View {
         var color : Color
         var name : String
         var description : String
+        var moods : String
     }
     
     //create object array of colors and names
     let colorArray = [
         RoomColor(
             color:Color(#colorLiteral(red: 0.8102983236, green: 0, blue: 0, alpha: 1)),
-            name: "Passion",
-            description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly."
+            name: "Red | Passion",
+            description: "Red has the slowest rate of vibration and the longest wavelength in the color spectrum. The color red is believed to help strengthen the body and internal organs.",
+            moods: "Courage. Drive. Energy. Passion. Strength. Tenacity."
         ),
         RoomColor(
-                  color:Color(#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)),
-                  name: "Driven",
-                  description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly."
-              ),
+            color:Color(#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)),
+            name: "Orange | Creativity",
+            description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly.",
+            moods: "Brave. Creative. Pleasure. Optimistic. Spontaneous. "
+        ),
         RoomColor(
-                  color:Color(#colorLiteral(red: 0.9886365533, green: 0.9814520478, blue: 0.3510926366, alpha: 1)),
-                  name: "Inspired",
-                  description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly."
-              ),
+            color:Color(#colorLiteral(red: 0.9886365533, green: 0.9814520478, blue: 0.3510926366, alpha: 1)),
+            name: "Yellow | Joy",
+            description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly.",
+            moods: "Enlightened. Humorous. Joy. Logical. Successful.  "
+        ),
         
         RoomColor(
             color:Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)),
-            name: "Soothing",
-            description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly."
+            name: "Green | Balance",
+            description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly.",
+            moods: "Accepting. Balance. Harmony. Love.  "
         ),
         RoomColor(
-                  color:Color(#colorLiteral(red: 0.08957818896, green: 0.233299017, blue: 0.7654716969, alpha: 1)),
-                  name: "Calming",
-                  description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly."
-              ),
+            color:Color(#colorLiteral(red: 0.08957818896, green: 0.233299017, blue: 0.7654716969, alpha: 1)),
+            name: "Blue | Calming",
+            description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly.",
+            moods: "Appreciative. Calm. Honest. Self-Expressive."
+        ),
         RoomColor(
-                   color:Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)),
-                   name: "Unity",
-                   description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly."
-               ),
-      
+            color:Color(#colorLiteral(red: 0.331466198, green: 0.1703749895, blue: 0.8207140565, alpha: 1)),
+            name: "Indigo | Wisdom",
+            description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly.",
+            moods: "Dignity. Maturity. Wisdom."
+        ),
+        
         RoomColor(
             color:Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)),
-            name: "Ambition",
-            description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly."
+            name: "Violet | Imagination",
+            description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly.",
+            moods: "Artistic. Knowledge. Imagination. Intuition. Meditation.  "
         ),
-      RoomColor(
-                 color:Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)),
-                 name: "Unity",
-                 description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly."
-             ),
-      RoomColor(
-                 color:Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)),
-                 name: "Unity",
-                 description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly."
-             ),
-      RoomColor(
-                 color:Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)),
-                 name: "Unity",
-                 description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly."
-             )
-      
-       
+        RoomColor(
+            color:Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)),
+            name: "White | Purity",
+            description: "White is not just a color, it's every color. ",
+            moods: "Balance. Harmony. Oneness. Purity. Truth. "
+        ),
+        RoomColor(
+            color:Color(#colorLiteral(red: 0.8110381961, green: 0, blue: 0.7638235688, alpha: 1)),
+            name: "Magenta | Creative",
+            description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly.",
+            moods: "Energy. Vitality. "
+        ),
+        RoomColor(
+            color:Color(#colorLiteral(red: 0.8181771636, green: 0.4467838407, blue: 0.8082491159, alpha: 1)),
+            name: "Pink",
+            description: "This is long text to see how I want to stylize all of this and eventually add a description saying what each color represents. It helps to have a lot of text to explain all of these things and see how to format it correctly.",
+            moods: "Energy. Vitality. "
+        )
+        
+        
     ]
     
     
